f81500e057c433254988b9d331db82682fc9c4b1
FredRaw <fredrikrawicki@gmail.com>
2025-10-14 10:13:09 +0200

    cleanup

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -4,10 +4,13 @@ import { parse } from 'csv-parse/sync'
 import { nameSplitter, genderSelector, hobbiesSelector } from '../utils/helpers/helperFunctions'
 
 test.beforeEach(async({page}) => {
+
     await page.goto('https://demoqa.com/automation-practice-form')
 })
+
 // Define the structure of your CSV data
 interface UserData {
+
     name: string,
     email: string,
     gender: string,
@@ -18,11 +21,13 @@ interface UserData {
     current_address: string,
     state: string,
     city: string
+    
 }
 
 
 // Read and parse the CSV file
 const records: UserData[] = parse(
+
     fs.readFileSync('pageModel/tomato_combinations.csv', 'utf-8'),
     {
         columns: true,
@@ -40,56 +45,62 @@ for (const record of records) {
 
     test('Test case: ' + counter, async ({page}) => {
 
-    const [firstName, lastName] = nameSplitter(record.name)
+        const [firstName, lastName] = nameSplitter(record.name)
 
-    await page.locator('#firstName').fill(firstName)
-    await page.locator('#lastName').fill(lastName)
+        await page.locator('#firstName').fill(firstName)
 
-    await page.locator('#userEmail').fill(record.email)
+        await page.locator('#lastName').fill(lastName)
 
-    //  -1 for male, -2 for female and -3 for other
-    const gender = genderSelector(record.gender)
+        await page.locator('#userEmail').fill(record.email)
 
-    await page.locator(gender).check()
+        //  -1 for male, -2 for female and -3 for other
+        const gender = genderSelector(record.gender)
 
-    await page.locator('#userNumber').fill(record.mobile)
+        await page.locator(gender).check()
 
-    await page.locator('#dateOfBirthInput').fill(record.birthdate)
+        await page.locator('#userNumber').fill(record.mobile)
 
-    // expand to multiple find multiple inputs..
-    await page.fill('input[id="subjectsInput"]', record.subjects)
-    await page.waitForSelector('.subjects-auto-complete__menu')
-    // await page.getByText(record.subjects).last().click()
-    await page.locator('#subjectsContainer > div.subjects-auto-complete__menu.css-26l3qy-menu').click()
-    
-    // -1 for sports, -2 for Reading, -3 for Music
-    const hobbies = hobbiesSelector(record.hobbies)
+        await page.locator('#dateOfBirthInput').fill(record.birthdate)
 
-    await page.locator(hobbies).check()
+        // expand to multiple find multiple inputs..
+        await page.fill('input[id="subjectsInput"]', record.subjects)
 
-    await page.locator('#currentAddress').fill(record.current_address)
+        await page.waitForSelector('.subjects-auto-complete__menu')
 
-    // find select state
-    await page.getByText('Select State').click()
+        // await page.getByText(record.subjects).last().click()
+        await page.locator('#subjectsContainer > div.subjects-auto-complete__menu.css-26l3qy-menu').click()
+        
+        // -1 for sports, -2 for Reading, -3 for Music
+        const hobbies = hobbiesSelector(record.hobbies)
 
-    await page.waitForSelector('#state > div.css-26l3qy-menu')
+        await page.locator(hobbies).check()
 
-    await page.getByText(record.state).last().click()
+        await page.locator('#currentAddress').fill(record.current_address)
 
-    // find select City
-    await page.getByText('Select City').click()
-    await page.waitForSelector('#city > div.css-26l3qy-menu')
-    await page.getByText(record.city).last().click()
+        // find select state
+        await page.getByText('Select State').click()
 
-    // finaly click the Submit button
-    await page.getByText('Submit').click()
+        await page.waitForSelector('#state > div.css-26l3qy-menu')
 
-    // wait for overlay to be displayed!
+        await page.getByText(record.state).last().click()
 
-    await page.waitForSelector('.modal-open')
+        // find select City
+        await page.getByText('Select City').click()
 
-    await page.waitForTimeout(1000)
-    
-    await page.mouse.click(10,10)
+        await page.waitForSelector('#city > div.css-26l3qy-menu')
+
+        await page.getByText(record.city).last().click()
+
+        // finaly click the Submit button
+        await page.getByText('Submit').click()
+
+        // wait for overlay to be displayed!
+
+        await page.waitForSelector('.modal-open')
+
+        await page.waitForTimeout(1000)
+        
+        await page.mouse.click(10,10)
+
     })
 }
--- utils/helpers/helperFunctions.ts
+++ utils/helpers/helperFunctions.ts
@@ -10,19 +10,31 @@ export function nameSplitter(fullname: string): [string, string] {
 }
 
 export function genderSelector(gender: string): string {
+
     if (gender == "Male") {
+
         return 'label[for="gender-radio-1"]'
+
     } else if (gender == "Female") {
+
         return 'label[for="gender-radio-2"]'
+
     } else  
+
         return 'label[for="gender-radio-3"]'
 }
 
 export function hobbiesSelector(hobby: string): string {
+
     if (hobby == "Sports") {
+
         return 'label[for="hobbies-checkbox-1"]'
+
     } else if (hobby == "Reading") {
+
         return 'label[for="hobbies-checkbox-2"]'
+
     } else  
+        
         return 'label[for="hobbies-checkbox-3"]'
 }
